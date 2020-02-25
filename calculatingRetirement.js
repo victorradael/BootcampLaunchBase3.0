1ecb042b09dc87ce07b7bb0c81f8b652da595bf5
Victor Radael <victorradael@hotmail.com>
2020-02-25 18:42:00 -0300

    Program that checks whether people on a list can retire

---
+++ calculatingRetirement.js
@@ -0,0 +1,66 @@
+const people = [
+    {
+        name: 'Silvana',
+        age: 48,
+        contributionInYears: 23,
+        genre: 'F'
+    },
+
+    {
+        name: 'Ricardo',
+        age: 75,
+        contributionInYears: 40,
+        genre: 'M'
+    },
+
+    {
+        name: 'Jussara',
+        age: 60,
+        contributionInYears: 33,
+        genre: 'F'
+    },
+
+    {
+        name: 'Manoel',
+        age: 55,
+        contributionInYears: 20,
+        genre: 'M'
+    }
+];
+
+function validateRetirement (person) {
+    if(person.genre === 'M') {
+        let sumService = person.age + person.contributionInYears
+        if(sumService > 95 && person.contributionInYears >= 35){
+            return person.retirement = true
+        } else {
+            return person.retirement = false
+        }
+
+    }else if(person.genre === 'F') {
+        let sumService = person.age + person.contributionInYears
+        if(sumService > 85 && person.contributionInYears >= 30){
+            return person.retirement = true
+        } else {
+            return person.retirement = false
+        }
+
+    } else {
+        console.log('Invalid Genre')
+    }
+};
+
+function sendMessageRetirement (people) {
+    for (const person of people) {
+        validateRetirement(person)
+        if(person.retirement){
+            console.log(`${person.name} can retire.`)
+        } else if (!person.retirement) {
+            console.log(`${person.name} can't retire.`)
+        } else {
+            console.log(' Invalid State')
+        }
+    }
+};
+
+sendMessageRetirement(people)
